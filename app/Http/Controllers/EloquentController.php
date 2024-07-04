d0f8e3754b22c7e6c4493b5ae0333a5986cef1b1
Shaundyl <shaundylalipio@gmail.com>
2024-07-04 13:20:06 +0800

    New Files

---
+++ app/Http/Controllers/EloquentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Eloquent;
+use App\Http\Requests\StoreEloquentRequest;
+use App\Http\Requests\UpdateEloquentRequest;
+
+class EloquentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEloquentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Eloquent $eloquent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Eloquent $eloquent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEloquentRequest $request, Eloquent $eloquent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Eloquent $eloquent)
+    {
+        //
+    }
+}
